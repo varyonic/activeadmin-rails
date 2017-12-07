80c2fb63e61b28be6b5bce84284cee7330641af7
Piers Chambers <piers@varyonic.com>
2017-12-07 15:15:06 -0500

    Use a feature test to verify body CSS classes and revert original unit test.

--- features/step_definitions/layout_steps.rb
+++ features/step_definitions/layout_steps.rb
@@ -1,3 +1,4 @@
 Then /^I should see the Active Admin layout$/ do
+  expect(page).to have_css 'body.active_admin.admin_namespace'
   expect(page).to have_css '#active_admin_content #main_content_wrapper'
 end
