fe7f5fc37804471a75fea19654185e2d7d94a39a
Piers Chambers <piers@varyonic.com>
2018-06-22 09:09:32 -0400

    bundle gem activeadmin-core rm activeadmin-core/.gitignore activeadmin-core/.travis.yml activeadmin-core/bin/console activeadmin-core/bin/setup activeadmin-core/test/test_helper.rb activeadmin-core/test/activeadmin/core_test.rb mv activeadmin-core/LICENSE.txt activeadmin-core/MIT-LICENSE.txt

---
+++ activeadmin-core/lib/activeadmin/core.rb
@@ -0,0 +1,7 @@
+require "activeadmin/core/version"
+
+module Activeadmin
+  module Core
+    # Your code goes here...
+  end
+end
---
+++ activeadmin-core/lib/activeadmin/core/version.rb
@@ -0,0 +1,5 @@
+module Activeadmin
+  module Core
+    VERSION = "0.1.0"
+  end
+end
