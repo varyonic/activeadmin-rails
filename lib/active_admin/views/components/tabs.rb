activeadmin/activeadmin

Add workaround for non-transliteratable tab titles (#5650)
Fix code that generates fragment link and id for tab content for non-transliteratable tab titles by using URL-encoded string representation.

lib/active_admin/views/components/tabs.rb
@@ -15,7 +15,8 @@ module ActiveAdmin
       end
 
       def build_menu_item(title, options, &block)
-        fragment = options.fetch(:id, title.parameterize)
+        fragment = options.fetch(:id, fragmentize(title))
+
         html_options = options.fetch(:html_options, {})
         li html_options do
           link_to title, "##{fragment}"
@@ -23,8 +24,16 @@ module ActiveAdmin
       end
 
       def build_content_item(title, options, &block)
-        options = options.reverse_merge(id: title.parameterize)
+        options = options.reverse_merge(id: fragmentize(title))
         div(options, &block)
+      end
+
+      private
+
+      def fragmentize(string)
+        result = string.parameterize
+        result = URI.encode(string) if result.blank?
+        result
       end
     end
   end

spec/unit/views/components/tabs_spec.rb
@@ -59,11 +59,38 @@ RSpec.describe ActiveAdmin::Views::Tabs do
       end
 
       it "should create a tab navigation bar based on the symbol" do
-        expect(tabs.find_by_tag('li').first.content).to include "Overview"
+        expect(tabs.find_by_tag('li').first.content).to include("Overview")
       end
 
       it "should create a tab with a span inside of it" do
         expect(tabs.find_by_tag('span').first.content).to eq('tab 1')
+      end
+    end
+
+    context "when creating a tab with non-transliteratable string" do
+      let(:tabs) do
+        render_arbre_component do
+          tabs do
+            tab '🤗' do
+              'content'
+            end
+          end
+        end
+      end
+
+      let(:subject) { Capybara.string(tabs.to_s) }
+
+      it "should create a tab navigation bar based on the string" do
+        expect(subject).to have_content('🤗')
+      end
+
+      it "should have tab with id based on URL-encoded string" do
+        tab_content = subject.find('.tabs .tab-content div', text: 'content')
+        expect(tab_content['id']).to eq(URI.encode('🤗'))
+      end
+
+      it "should have link with fragment based on URL-encoded string" do
+        expect(subject).to have_link('🤗', href: "##{URI.encode('🤗')}")
       end
     end
   end

varyonic/activeadmin-rails

lib/active_admin/views/components/tabs.rb
@@ -15,7 +15,8 @@ module ActiveAdmin
       end
 
       def build_menu_item(title, options, &block)
-        fragment = options.fetch(:id, title.parameterize)
+        fragment = options.fetch(:id, fragmentize(title))
+
         html_options = options.fetch(:html_options, {})
         li html_options do
           link_to title, "##{fragment}"
@@ -23,8 +24,16 @@ module ActiveAdmin
       end
 
       def build_content_item(title, options, &block)
-        options = options.reverse_merge(id: title.parameterize)
+        options = options.reverse_merge(id: fragmentize(title))
         div(options, &block)
+      end
+
+      private
+
+      def fragmentize(string)
+        result = string.parameterize
+        result = CGI.escape(string) if result.blank?
+        result
       end
     end
   end

spec/unit/views/components/tabs_spec.rb
@@ -60,11 +60,38 @@ RSpec.describe ActiveAdmin::Views::Tabs do
       end
 
       it "should create a tab navigation bar based on the symbol" do
-        expect(tabs.find_by_tag('li').first.content).to include "Overview"
+        expect(tabs.find_by_tag('li').first.content).to include("Overview")
       end
 
       it "should create a tab with a span inside of it" do
         expect(tabs.find_by_tag('span').first.content).to eq('tab 1')
+      end
+    end
+
+    context "when creating a tab with non-transliteratable string" do
+      let(:tabs) do
+        render_arbre_component do
+          tabs do
+            tab '🤗' do
+              'content'
+            end
+          end
+        end
+      end
+
+      let(:subject) { Capybara.string(tabs.to_s) }
+
+      it "should create a tab navigation bar based on the string" do
+        expect(subject).to have_content('🤗')
+      end
+
+      it "should have tab with id based on URL-encoded string" do
+        tab_content = subject.find('.tabs .tab-content div', text: 'content')
+        expect(tab_content['id']).to eq(CGI.escape('🤗'))
+      end
+
+      it "should have link with fragment based on URL-encoded string" do
+        expect(subject).to have_link('🤗', href: "##{CGI.escape('🤗')}")
       end
     end
   end