Xlebushek/activeadmin

Move setup task to a better place

tasks/test.rake
@@ -1,5 +1,12 @@
+require_relative "application_generator"
+
 desc "Run the full suite using 1 core"
 task test: [:spec, :cucumber]
+
+desc 'Creates a test rails app for the specs to run against'
+task :setup, [:parallel, :dir, :template] do |_t, opts|
+  ActiveAdmin::ApplicationGenerator.new(opts).generate
+end
 
 desc "Run the specs"
 task :spec do

varyonic/activeadmin-rails

tasks/test.rake
@@ -1,5 +1,12 @@
+require_relative "application_generator"
+
 desc "Run the full suite using 1 core"
 task test: [:spec, :cucumber]
+
+desc 'Creates a test rails app for the specs to run against'
+task :setup, [:parallel, :rails_env, :template] do |_t, opts|
+  ActiveAdmin::ApplicationGenerator.new(opts).generate
+end
 
 desc "Run the specs"
 task :spec do